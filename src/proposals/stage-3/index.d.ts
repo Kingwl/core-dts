0bb89002d87b9fbe790fd2dbdcad575fcabe7920
kingwl <kingwenlu@gmail.com>
2022-08-01 19:54:46 +0800

    fix stage 3

--- src/proposals/stage-3/index.d.ts
+++ src/proposals/stage-3/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../stage-4/index.d.ts" />
 
-/// <reference path="./array-grouping/index.d.ts" />
-/// <reference path="./change-array-by-copy/index.d.ts" />
+/// <reference path="./array-grouping.d.ts" />
+/// <reference path="./change-array-by-copy.d.ts" />
