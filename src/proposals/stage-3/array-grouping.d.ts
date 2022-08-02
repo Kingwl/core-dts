4ea4085e19c115eee549fbd7e243995f36ebc807
kingwl <kingwenlu@gmail.com>
2022-08-02 11:18:05 +0800

    format

--- src/proposals/stage-3/array-grouping.d.ts
+++ src/proposals/stage-3/array-grouping.d.ts
@@ -7,7 +7,7 @@ interface Array<T> {
   groupToMap<S extends T>(
     callback: (value: T, index: number, obj: T) => value is S,
     thisArg?: any,
-  ): Map<true, S[]> & Map<false, (Exclude<T, S>)[]>;
+  ): Map<true, S[]> & Map<false, Exclude<T, S>[]>;
   groupToMap<K>(
     callback: (value: T, index: number, obj: T[]) => K,
     thisArg?: any,
