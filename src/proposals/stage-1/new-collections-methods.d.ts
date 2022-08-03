5a8c17efc51f418d9565ed1c0e06499aa3d8d2f1
kingwl <kingwenlu@gmail.com>
2022-08-03 15:10:35 +0800

    fix collections

--- src/proposals/stage-1/new-collections-methods.d.ts
+++ src/proposals/stage-1/new-collections-methods.d.ts
@@ -1,63 +1,85 @@
 interface Set<T> {
-    addAll(...args: Array<T>): this;
-    deleteAll(...args: Array<T>): boolean;
+  addAll(...args: Array<T>): this;
+  deleteAll(...args: Array<T>): boolean;
 
-    every<S extends T>(callback: (value: T) => value is S, thisArg?: any): this is Set<S>;
-    every(callback: (value: T) => unknown, thisArg?: any): boolean;
+  every<S extends T>(
+    callback: (value: T) => value is S,
+    thisArg?: any,
+  ): this is Set<S>;
+  every(callback: (value: T) => unknown, thisArg?: any): boolean;
 
-    filter<S extends T>(callback: (value: T) => value is S, thisArg?: any): Set<S>;
-    filter(callback: (value: T) => unknown, thisArg?: any): Set<T>;
+  filter<S extends T>(
+    callback: (value: T) => value is S,
+    thisArg?: any,
+  ): Set<S>;
+  filter(callback: (value: T) => unknown, thisArg?: any): Set<T>;
 
-    find<S extends T>(callback: (value: T) => value is S, thisArg?: any): S | undefined;
-    find(callback: (value: T) => unknown, thisArg?: any): T | undefined;
+  find<S extends T>(
+    callback: (value: T) => value is S,
+    thisArg?: any,
+  ): S | undefined;
+  find(callback: (value: T) => unknown, thisArg?: any): T | undefined;
 
-    join(separator?: string): string;
+  join(separator?: string): string;
 
-    map<U>(callback: (value: T) => U, thisArg?: any): Set<U>;
+  map<U>(callback: (value: T) => U, thisArg?: any): Set<U>;
 
-    reduce<U>(callback: (memo: U, value: T) => U): U | undefined;
-    reduce<U>(callback: (memo: U, value: T) => U, initialValue: U): U;
+  reduce<U>(callback: (memo: U, value: T) => U): U | undefined;
+  reduce<U>(callback: (memo: U, value: T) => U, initialValue: U): U;
 
-    some(callback: (value: T) => unknown, thisArg?: any): boolean;
+  some(callback: (value: T) => unknown, thisArg?: any): boolean;
 }
 
 interface MapConstructor {
-    groupBy<T, K>(iterable: Iterable<T>, callback: (value: T) => K): Map<K, T[]>
-    keyBy<T, K>(callback: (value: T) => K): Map<string, T>;
+  groupBy<T, K>(iterable: Iterable<T>, callback: (value: T) => K): Map<K, T[]>;
+  keyBy<T, K>(iterable: Iterable<T>, callback: (value: T) => K): Map<K, T>;
 }
 
 interface Map<K, V> {
-    deleteAll(...args: Array<K>): boolean;
+  deleteAll(...args: Array<K>): boolean;
 
-    every(callback: (value: V, key: K) => boolean, thisArg?: any): boolean;
+  every(callback: (value: V, key: K) => boolean, thisArg?: any): boolean;
 
-    filter(callback: (value: V, key: K) => boolean, thisArg?: any): Map<K, V>;
+  filter(callback: (value: V, key: K) => boolean, thisArg?: any): Map<K, V>;
 
-    find(callback: (value: V, key: K) => boolean, thisArg?: any): V | undefined;
+  find(callback: (value: V, key: K) => boolean, thisArg?: any): V | undefined;
 
-    findKey(callback: (value: V, key: K) => boolean, thisArg?: any): K | undefined;
+  findKey(
+    callback: (value: V, key: K) => boolean,
+    thisArg?: any,
+  ): K | undefined;
 
-    includes(searchElement: V): boolean;
-    keyOf(searchElement: V): K | undefined;
+  includes(searchElement: V): boolean;
+  keyOf(searchElement: V): K | undefined;
 
-    mapKeys<K1>(mapFn: (value: K, index: number) => K1, thisArg?: any): Map<K1, V>;
-    mapValues<V1>(mapFn: (value: V, index: number) => V1, thisArg?: any): Map<K, V1>;
+  mapKeys<K1>(
+    mapFn: (value: K, index: number) => K1,
+    thisArg?: any,
+  ): Map<K1, V>;
+  mapValues<V1>(
+    mapFn: (value: V, index: number) => V1,
+    thisArg?: any,
+  ): Map<K, V1>;
 
-    merge(...iterables: [K, V][]): this;
+  merge(...iterables: [K, V][]): this;
 
-    reduce<U>(callback: (memo: U, value: V, key: K) => U): U | undefined;
-    reduce<U>(callback: (memo: U, value: V, key: K) => U, initialValue: U): U;
+  reduce<U>(callback: (memo: U, value: V, key: K) => U): U | undefined;
+  reduce<U>(callback: (memo: U, value: V, key: K) => U, initialValue: U): U;
 
-    some(callback: (value: V, key: K) => boolean, thisArg?: any): boolean;
+  some(callback: (value: V, key: K) => boolean, thisArg?: any): boolean;
 
-    update(key: K, callback: (value: V, key: K) => V, thunk?: (key: K) => K): this;
+  update(
+    key: K,
+    callback: (value: V, key: K) => V,
+    thunk?: (key: K) => V,
+  ): this;
 }
 
 interface WeakSet<T> {
-    addAll(...args: Array<T>): this;
-    deleteAll(...args: Array<T>): boolean;
+  addAll(...args: Array<T>): this;
+  deleteAll(...args: Array<T>): boolean;
 }
 
 interface WeakMap<K, V> {
-    deleteAll(...args: Array<K>): boolean;
+  deleteAll(...args: Array<K>): boolean;
 }
--- src/proposals/stage-1/of-from-methods-on-collections.d.ts
+++ src/proposals/stage-1/of-from-methods-on-collections.d.ts
@@ -1,27 +1,43 @@
 interface SetConstructor {
-    of<T>(...items: T[]): Set<T>
+  of<T>(...items: T[]): Set<T>;
 
-    from<T>(iterable: Iterable<T>): Set<T>;
-    from<T, S>(iterable: Iterable<T>, mapFn: (value: T, index: number) => S, thisArg?: any): Set<S>;
+  from<T>(iterable: Iterable<T>): Set<T>;
+  from<T, S>(
+    iterable: Iterable<T>,
+    mapFn: (value: T, index: number) => S,
+    thisArg?: any,
+  ): Set<S>;
 }
 
 interface MapConstructor {
-    of<K, V>(...items: [K, V][]): Map<K, V>
+  of<K, V>(...items: [K, V][]): Map<K, V>;
 
-    from<K, V>(iterable: Iterable<[K, V]>): Map<K, V>;
-    from<K, V, K1, V1>(iterable: Iterable<[K, V]>, mapFn: (value: [K, V], index: number) => [K1, V1], thisArg?: any): Map<K1, V1>;
+  from<K, V>(iterable: Iterable<[K, V]>): Map<K, V>;
+  from<K, V, K1, V1>(
+    iterable: Iterable<[K, V]>,
+    mapFn: (value: [K, V], index: number) => [K1, V1],
+    thisArg?: any,
+  ): Map<K1, V1>;
 }
 
 interface WeakSetConstructor {
-    of<T extends object>(...items: T[]): WeakSet<T>
+  of<T extends object>(...items: T[]): WeakSet<T>;
 
-    from<T extends object>(iterable: Iterable<T>): WeakSet<T>;
-    from<T, S extends object>(iterable: Iterable<T>, mapFn: (value: T, index: number) => S, thisArg?: any): WeakSet<S>;
+  from<T extends object>(iterable: Iterable<T>): WeakSet<T>;
+  from<T, S extends object>(
+    iterable: Iterable<T>,
+    mapFn: (value: T, index: number) => S,
+    thisArg?: any,
+  ): WeakSet<S>;
 }
 
 interface WeakMapConstructor {
-    of<K extends object, V>(...items: [K, V][]): WeakMap<K, V>
+  of<K extends object, V>(...items: [K, V][]): WeakMap<K, V>;
 
-    from<K extends object, V>(iterable: Iterable<[K, V]>): WeakMap<K, V>;
-    from<K, V, K1 extends object, V1>(iterable: Iterable<[K, V]>, mapFn: (value: [K, V], index: number) => [K1, V1], thisArg?: any): WeakMap<K1, V1>;
+  from<K extends object, V>(iterable: Iterable<[K, V]>): WeakMap<K, V>;
+  from<K, V, K1 extends object, V1>(
+    iterable: Iterable<[K, V]>,
+    mapFn: (value: [K, V], index: number) => [K1, V1],
+    thisArg?: any,
+  ): WeakMap<K1, V1>;
 }
